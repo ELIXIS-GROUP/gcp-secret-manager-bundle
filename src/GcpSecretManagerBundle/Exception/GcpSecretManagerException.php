eb74322980a3012fd8e6cb2ca4be5673d1cad32b
biqette <anthony.papillaud@gmail.com>
2021-12-08 17:26:25 +0100

    Delete src/GcpSecretManagerBundle directory

--- src/GcpSecretManagerBundle/Exception/GcpSecretManagerException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace GcpSecretManager\GcpSecretManagerBundle\Exception;
-
-use Symfony\Component\Config\Definition\Exception\Exception;
-
-class GcpSecretManagerException extends Exception{}
